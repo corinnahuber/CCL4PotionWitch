61f75b4bdd15db824c6d86c9e210c50d38d91371
user231045 <cc231045@fhstp.ac.at>
2025-06-20 19:03:34 +0200

    wwise audios implementation

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BOOKFLIP = 2136059192U;
+        static const AkUniqueID PLAY_CAULDRON = 3212638512U;
+        static const AkUniqueID PLAY_CAVE = 3602995889U;
+        static const AkUniqueID PLAY_FOOTSTEPSINSIDE = 4132445999U;
+        static const AkUniqueID PLAY_FOOTSTEPSOUTSIDE = 2713974140U;
+        static const AkUniqueID PLAY_FOREST = 207755397U;
+        static const AkUniqueID PLAY_GHOSTIDLE = 3011067769U;
+        static const AkUniqueID PLAY_GRAVEYARD = 315336149U;
+        static const AkUniqueID PLAY_LAKE = 40648359U;
+        static const AkUniqueID PLAY_MAINMENU = 3738780720U;
+        static const AkUniqueID PLAY_PICKUP = 3860455926U;
+        static const AkUniqueID STOP_MAINMENU = 890527358U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BOOKFLIP_SOUNDBANK = 3153541569U;
+        static const AkUniqueID CAULDRON_SOUNDBANK = 3772148165U;
+        static const AkUniqueID CAVE_SOUNDBANK = 4106907856U;
+        static const AkUniqueID FOOTSTEPSINSIDE_SOUNDBANK = 2179242980U;
+        static const AkUniqueID FOOTSTEPSOUTSIDE_SOUNDBANK = 2841106693U;
+        static const AkUniqueID FOREST_SOUNDBANK = 4002332272U;
+        static const AkUniqueID GHOSTATTACKED_SOUNDBANK = 894278887U;
+        static const AkUniqueID GHOSTIDLE_SOUNDBANK = 1084749322U;
+        static const AkUniqueID GRAVEYARD_SOUNDBANK = 3831665202U;
+        static const AkUniqueID LAKE_SOUNDBANK = 1256153282U;
+        static const AkUniqueID MAINMENU_SOUNDBANK = 3751373289U;
+        static const AkUniqueID PICKUP_SOUNDBANK = 457915711U;
+        static const AkUniqueID PLAYERATTACKED_SOUNDBANK = 1954416035U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
